d5a5eab702adf17f2c272620a1593d04fc4ba827
noverryjs <noverryambo@gmail.com>
2026-04-02 09:52:29 +0700

    unit test number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::arch::aarch64::int32x2_t;
+
 fn main() {
     println!("Hello, world!");
 }
@@ -24,4 +26,52 @@ fn hello_world(){
 
     print!("HELLO");
     print!("HELLO");
+}
+
+#[test]
+fn test_variables(){
+    let x = 5;
+    let y = 10;
+    let name = "Bambang Soeharto";
+    println!("x: {}, y: {}, z: {}", x, y, x + y);
+    println!("name : {}", name);
+
+}
+
+#[test]
+fn test_shadowing(){
+    let name = "Bambang Soharto";
+    println!("name: {}", name);
+
+    let name = 1234;
+    println!("name: {}", name);
+
+
+}
+
+
+#[test]
+fn test_comment(){
+    // contoh comment
+
+    /*comment lagi*/
+
+    /*coba lagi
+    comment*/
+}
+
+#[test]
+fn data_type(){
+    let age: i32 = 18;
+    println!("age: {}", age);
+}
+
+#[test]
+fn number(){
+    let age: i32 = 18;
+    println!("age: {}", age);
+
+
+    let b: f64 = 10.2;
+    println!("b: {}", b);
 }
