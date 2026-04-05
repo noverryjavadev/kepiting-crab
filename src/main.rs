10224c498b5b92d0ebaf00c1089428bd9219f00d
noverryjs <noverryambo@gmail.com>
2026-04-05 22:32:30 +0700

    push lagi

--- src/main.rs
+++ src/main.rs
@@ -119,3 +119,116 @@ fn comparisson_operator(){
     let c = a > b;
     println!("{}", c);
 }
+
+#[test]
+fn augmented_assignment() {
+    let mut a = 10;
+    println!("{}", a);
+
+    a += 10;
+    println!("{}", a);
+
+    a -= 10;
+    println!("{}", a);
+}
+
+#[test]
+fn boolean() {
+    let a = true;
+    let b: bool = false;
+
+    println!("{} {}", a, b);
+}
+
+#[test]
+fn boolean_operator() {
+    let absen = 75;
+    let nilai_akhir = 80;
+
+    let lulus_absen: bool = absen >= 75;
+    let lulus_nilai_akhir: bool = nilai_akhir >= 75;
+
+    let lulus: bool = lulus_absen && lulus_nilai_akhir;
+    println!("{}", lulus);
+}
+
+#[test]
+fn char_type(){
+    let char1 : char = 'a';
+    let char2 : char = 'b';
+    println!("char1: {}, char2: {}", char1, char2);
+}
+
+#[test]
+fn tuple() {
+    let mut data: (i32, f64, bool) = (10, 10.5, true);
+    println!("{:?}", data);
+
+    // let a = data.0;
+    // let b = data.1;
+    // let c = data.2;
+
+    let (a, b, c) = data;
+    println!("{} {} {}", a, b, c);
+
+    data.0 = 20;
+    data.1 = 20.5;
+    data.2 = false;
+    println!("{:?}", data);
+}
+
+fn unit() {
+    println!("Hello");
+}
+
+#[test]
+fn test_unit() {
+    let result: () = unit();
+    println!("{:?}", result);
+
+    let test: () = ();
+    println!("{:?}", test);
+}
+
+#[test]
+fn array() {
+    let mut array: [i32; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", array);
+
+    let a = array[0];
+    let b = array[1];
+    println!("{} {}", a, b);
+
+    array[0] = 10;
+    array[1] = 20;
+    println!("{:?}", array);
+
+    let length = array.len();
+    println!("{}", length);
+}
+
+#[test]
+fn two_dimensional_array() {
+    let matrix: [[i32; 3]; 2] = [
+        [1, 2, 3],
+        [4, 5, 6]
+    ];
+
+    println!("{:?}", matrix);
+    println!("{:?}", matrix[0]);
+    println!("{:?}", matrix[0][0]);
+    println!("{:?}", matrix[0][1]);
+    println!("{:?}", matrix[0][2]);
+    println!("{:?}", matrix[1]);
+    println!("{:?}", matrix[1][0]);
+    println!("{:?}", matrix[1][1]);
+    println!("{:?}", matrix[1][2]);
+}
+
+const MAXIMUM: i32 = 100;
+
+#[test]
+fn constant() {
+    const MINIMUM: i32 = 0;
+    println!("{} {}", MINIMUM, MAXIMUM);
+}
