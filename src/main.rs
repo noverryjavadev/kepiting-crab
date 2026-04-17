605b60d67e3f2a76408c50828e8ee7c947fc59b0
noverryjs <noverryambo@gmail.com>
2026-04-17 21:34:19 +0700

    push main kewajiban

--- src/main.rs
+++ src/main.rs
@@ -517,3 +517,30 @@ fn print_text(value: String, times: u32) {
 
     print_text(value, times - 1);
 }
+
+#[test]
+fn test_print_text() {
+    print_text(String::from("Eko"), 10);
+}
+
+fn factorial_recursive(n: u32) -> u32 {
+    if n <= 1 {
+        return 1;
+    }
+
+    n * factorial_recursive(n - 1)
+}
+
+#[test]
+fn test_factorial_recursive() {
+    let result = factorial_recursive(5);
+    println!("{}", result);
+}
+
+fn print_number(number: i32) {
+    println!("number {}", number);
+}
+
+fn hi(name: String) {
+    println!("name {}", name);
+}
